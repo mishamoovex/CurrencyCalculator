7d2e5f6530179882b6954cc5ab7e22e820a5d55e
Mykhailo Vasylenko <mvasylenko@solvd.com>
2022-11-28 13:56:10 +0200

    Currency VM logic

--- feature/currency/src/main/java/com/mykhailo/vasylenko/feature/currency/ui/screen/CurrencyVM.kt
+++ feature/currency/src/main/java/com/mykhailo/vasylenko/feature/currency/ui/screen/CurrencyVM.kt
@@ -2,15 +2,79 @@ package com.mykhailo.vasylenko.feature.currency.ui.screen
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mykhailo.vasylenko.common.exeption.SnackbarMessage
+import com.mykhailo.vasylenko.common.state.MessageState
+import com.mykhailo.vasylenko.common.vm.executeAction
 import com.mykhailo.vasylenko.core.models.ExchangeItemType
+import com.mykhailo.vasylenko.dispatchers.DispatcherIo
+import com.mykhailo.vasylenko.feature.currency.data.CurrencyRepository
+import com.mykhailo.vasylenko.feature.currency.domain.ExchangeCurrency
+import com.mykhailo.vasylenko.feature.currency.ui.state.CurrencyScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class CurrencyVM @Inject constructor(
+    @DispatcherIo private val ioDispatcher: CoroutineDispatcher,
+    private val repository: CurrencyRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val currencyType: ExchangeItemType =
         ExchangeItemType.valueOf(checkNotNull(savedStateHandle["type"]))
+
+    private val messageState = MutableStateFlow(
+        MessageState(
+            message = null,
+            onMessageShowed = { setMessage(null) }
+        )
+    )
+
+    private val currencies = MutableStateFlow<List<ExchangeCurrency>>(listOf())
+
+    val screenState: StateFlow<CurrencyScreenState> = combine(
+        messageState,
+        currencies
+    ) { m, c ->
+        CurrencyScreenState(
+            messageState = m,
+            currencies = c,
+            type = currencyType
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = CurrencyScreenState(
+            messageState = messageState.value,
+            currencies = currencies.value,
+            type = currencyType
+        )
+    )
+
+
+    init {
+        getCurrencies()
+    }
+
+    private fun getCurrencies() {
+        executeAction(
+            dispatcher = ioDispatcher,
+            onError = { setMessage(it) },
+            toDo = {
+                repository.getCurrencies()
+                    .also {
+                        currencies.emit(it)
+                    }
+            }
+        )
+    }
+
+    private fun setMessage(message: SnackbarMessage?) {
+        messageState.update {
+            it.copy(message = message)
+        }
+    }
 }
